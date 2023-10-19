afa004c9618349eb7beef33fb64a6f4c6d3e1a08
Thayna Lima <cristinathayna06@gmail.com>
2023-10-19 12:34:19 -0300

    Atualizar noticia no small news e dataFake

--- ANGULAR/Desafio - Blog com Angular/blog/src/app/data/dataFake.ts
+++ ANGULAR/Desafio - Blog com Angular/blog/src/app/data/dataFake.ts
@@ -8,16 +8,9 @@ export const dataFake = [
     },
     {
         "id": "2",
-        "photo": "https://www.videogameschronicle.com/files/2023/06/Switch_Pikmin4_Preview_BRoll_SCRN39.jpg",
-        "title": "Pikmin 4",
-        "description": "Pikmin 4 é um jogo eletrônico de estratégia em tempo real desenvolvido pela Nintendo EPD e publicado pela Nintendo. É o quarto jogo principal da série Pikmin, depois de Pikmin 3, e o sexto jogo no geral. Foi lançado no Nintendo Switch em 21 de julho de 2023. Fonte: Wikipédia",
-        "descPhoto": "Pikmin 4 imagem que vem da review de Video Games Chronicle"
+        "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSVW6ISgdBaizjoONi9VsTYMmbl2ngHhDS6peiNn6Kc5OnRBPTzmeFdmxCroZXXU4f2HI4&usqp=CAU",
+        "title": "Disney fez o filme elementos inspirado no jogo Fireboy & Watergirl: Elements ?",
+        "description": "Quem lembra do jogo Fireboy & Watergirl: Elements ou o jogo do Fogo e da água? Por incrivel que pareça somente suas semelhanças são visuais, mas foi uma comparação interesante porém o tema do jogo e do filme são muitos diferentes já que o jogo uma jornada mágica através de templos desafiadores, repletos de quebra-cabeças e obstáculos ",
+        "descPhoto": "Imagem do filme Elementos"
     },
-    {
-        "id": "3",
-        "photo": "https://www.videogameschronicle.com/files/2023/06/Switch_Pikmin4_Preview_BRoll_SCRN39.jpg",
-        "title": "Pikmin 4",
-        "description": "Pikmin 4 é um jogo eletrônico de estratégia em tempo real desenvolvido pela Nintendo EPD e publicado pela Nintendo. É o quarto jogo principal da série Pikmin, depois de Pikmin 3, e o sexto jogo no geral. Foi lançado no Nintendo Switch em 21 de julho de 2023. Fonte: Wikipédia",
-        "descPhoto": "Pikmin 4 imagem que vem da review de Video Games Chronicle"
-    }
 ]
